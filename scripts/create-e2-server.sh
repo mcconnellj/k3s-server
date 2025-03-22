e1e67bc5b1e68bd4586413c73eb86a3482319cfe
josh.v.mcconnell@gmail.com <josh.v.mcconnell@gmail.com>
2025-03-22 16:59:56 -0400

    test

---
+++ scripts/create-e2-server.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Run this script curl -sSL https://raw.githubusercontent.com/mcconnellj/k3s-server/main/scripts/create-e2-server.sh | bash
+
+# Load environment variables from the .env file if needed
+source .env
+
+# Create the VM with dynamic variables
+gcloud compute instances create $INSTANCE_NAME \
+    --project=$GCP_PROJECT \
+    --zone=$GCP_ZONE \
+    --machine-type=$MACHINE_TYPE \
+    --network-interface=network-tier=STANDARD,stack-type=IPV4_ONLY,subnet=default \
+    --provisioning-model=SPOT \
+    --instance-termination-action=STOP \
+    --service-account=$SERVICE_ACCOUNT \
+    --scopes=$SCOPES\
+    --tags=http-server,https-server \
+    --create-disk=auto-delete=yes,boot=yes,device-name=instance-20250321-180030,image=projects/ubuntu-os-cloud/global/images/ubuntu-2004-focal-v20250313,mode=rw,size=15,type=pd-balanced \
+    --no-shielded-secure-boot \
+    --shielded-vtpm \
+    --shielded-integrity-monitoring \
+    --labels=goog-ec-src=vm_add-gcloud \
+    --reservation-affinity=any \--metadata=startup-script-url=https://raw.githubusercontent.com/mcconnellj/k3s-server/main/scripts/install-k3s-agent.sh
--- scripts/install-k3s.sh
+++ scripts/install-k3s.sh
@@ -1,21 +1,9 @@
 #!/bin/bash
 
-set -e
-
 #curl -fsSL https://raw.githubusercontent.com/mcconnellj/k3s-server/scripts/install-k3s.sh | sh -
 
-# Set variables
-CONFIG_URL="https://raw.githubusercontent.com/mcconnellj/k3s-server/config/k3s/config.yaml"
-CONFIG_DIR="/etc/rancher/k3s"
-CONFIG_FILE="${CONFIG_DIR}/config.yaml"
+mkdir -p /var/lib/rancher/k3s/server/manifests/argocd
+curl -o /var/lib/rancher/k3s/server/manifests/argocd.yaml https://raw.githubusercontent.com/mcconnellj/k3s-server/main/manifests/argocd/argocd.yaml
+##curl -o /var/lib/rancher/k3s/server/manifests/phase-one-applicationset.yaml https://raw.githubusercontent.com/mcconnellj/k3s-server/main/manifests/phase-one-applicationset.yaml
 
-echo "[+] Creating config directory if it doesn't exist"
-sudo mkdir -p "$CONFIG_DIR"
-
-echo "[+] Downloading K3s config file from $CONFIG_URL"
-sudo wget -qO "$CONFIG_FILE" "$CONFIG_URL"
-
-echo "[+] Installing K3s with the downloaded config file"
 curl -sfL https://get.k3s.io | sh -
-
-echo "[+] K3s installation complete!"
