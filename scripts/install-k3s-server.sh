dfbc62b7451f021ed5d81fdfea36bd23103997d7
josh.v.mcconnell@gmail.com <josh.v.mcconnell@gmail.com>
2025-03-23 01:37:57 -0400

    test

--- scripts/install-k3s-server.sh
+++ scripts/install-k3s-server.sh
@@ -2,6 +2,8 @@
 
 #curl -fsSL https://raw.githubusercontent.com/mcconnellj/k3s-server/scripts/install-k3s.sh | sh -
 
+sudo apt update && sudo apt upgrade -y
+
 mkdir -p /var/lib/rancher/k3s/server/manifests/argocd
 curl -o /var/lib/rancher/k3s/server/manifests/argocd.yaml https://raw.githubusercontent.com/mcconnellj/k3s-server/main/manifests/argocd/argocd.yaml
 ##curl -o /var/lib/rancher/k3s/server/manifests/phase-one-applicationset.yaml https://raw.githubusercontent.com/mcconnellj/k3s-server/main/manifests/phase-one-applicationset.yaml
