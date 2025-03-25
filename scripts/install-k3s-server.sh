fc0143c9fcf2cd6cf09c225252b817f4952a25a2
josh.v.mcconnell@gmail.com <josh.v.mcconnell@gmail.com>
2025-03-25 14:03:39 -0400

    test

--- scripts/install-k3s-server.sh
+++ scripts/install-k3s-server.sh
@@ -4,11 +4,14 @@
 
 sudo apt update && sudo apt upgrade -y
 
-mkdir -p /var/lib/rancher/k3s/server/manifests/argocd
-curl -o /var/lib/rancher/k3s/server/manifests/argocd.yaml https://raw.githubusercontent.com/mcconnellj/k3s-server/main/manifests/argocd/argocd.yaml
-##curl -o /var/lib/rancher/k3s/server/manifests/phase-one-applicationset.yaml https://raw.githubusercontent.com/mcconnellj/k3s-server/main/manifests/phase-one-applicationset.yaml
+mkdir -p /var/lib/rancher/k3s/server/manifests/
+
+curl -L -o k3s-server.zip https://github.com/mcconnellj/k3s-server/archive/refs/heads/main.zip
+unzip k3s-server.zip & rm k3s-server.zip
+
+mv ./manifests/* /etc/rancher/k3s/server/manifests/
+mv ./config/k3s/* /var/lib/rancher/k3s/
 
 curl -sfL https://get.k3s.io | sh -
 curl -sS https://webinstall.dev/k9s | bash
-source ~/.config/envman/PATH.env
 
