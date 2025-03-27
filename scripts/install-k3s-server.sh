73a139c714a5e41eb3bdeea4f852bff59dd449db
josh.v.mcconnell@gmail.com <josh.v.mcconnell@gmail.com>
2025-03-27 13:49:29 -0400

    test

--- scripts/install-k3s-server.sh
+++ scripts/install-k3s-server.sh
@@ -13,18 +13,15 @@ if [[ -f /root/.local/bin/k9s ]]; then
     chmod +x /usr/local/bin/k9s
 fi
 
-# Create necessary directories
-mkdir -p /var/lib/rancher/k3s/server/manifests/
-mkdir -p /var/lib/rancher/k3s/
-
 # Download k3s manifests
 curl -L -o k3s-server.zip "https://github.com/mcconnellj/k3s-server/archive/refs/heads/main.zip?nocache=$(date +%s)"
 python3 -m zipfile -e k3s-server.zip .
 rm k3s-server.zip
 
-# Move manifests and config files
-mv ./k3s-server-main/manifests/ /etc/rancher/k3s/server/manifests/
-mv ./k3s-server-main/config/k3s/ /var/lib/rancher/k3s/
+mkdir -p /var/lib/rancher/k3s/server/manifests/
+mkdir -p /etc/rancher/k3s/
+mv ./k3s-server-main/manifests/ /var/lib/rancher/k3s/server/manifests/
+mv ./k3s-server-main/config/k3s/ /etc/rancher/k3s/
 
 # Install K3s
 curl -sfL https://get.k3s.io | sh -
