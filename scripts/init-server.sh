3ed1ee4466162d953a94cedc30c137ee1641b2cb
josh.v.mcconnell@gmail.com <josh.v.mcconnell@gmail.com>
2025-04-27 14:47:20 -0400

    testing

--- scripts/init-server.sh
+++ scripts/init-server.sh
@@ -19,15 +19,43 @@ mv ./k3s-server-main/configs/k3s/* /etc/rancher/k3s/
 # Install K3s
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server" sh -s
 
+# Get the current machines ip
+export SETUP_NODEIP=$(ip -4 addr show ens18 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+
+# Install K3s with the configuration file.
+curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="v1.32.2+k3s1" \
+  INSTALL_K3S_EXEC="--node-ip \$SETUP_NODEIP" \
+  K3S_TOKEN=\$SETUP_CLUSTERTOKEN \
+  K3S_CONFIG_FILE=./config.yaml \
+  sh -s -
+
 # Setup Kube Config
-sudo chmod 600 /etc/rancher/k3s/k3s.yaml
-echo "export KUBECONFIG=/etc/rancher/k3s/k3s.yaml" >> $HOME/.bashrc
-source $HOME/.bashrc
+mkdir -p $HOME/.kube && sudo cp -i /etc/rancher/k3s/k3s.yaml $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config && chmod 600 $HOME/.kube/config
+
+# Helm install Cilium
+helm repo add cilium https://helm.cilium.io
+helm repo update
+helm install cilium cilium/cilium -n kube-system \
+  -f applications/cilium/values.yaml \
+  -f applications/cilium/development.yaml \
+  --version 1.17.0-rc.2 \
+  --set operator.replicas=1
 
 # Install K9s
 curl -L -o k9s_linux_amd64.deb https://github.com/derailed/k9s/releases/download/v0.32.5/k9s_linux_amd64.deb
 sudo apt install ./k9s_linux_amd64.deb
 rm k9s_linux_amd64.deb
+
+# Wait for Argo CD
+kubectl wait --for=condition=Ready pod -l app.kubernetes.io/name=argocd-server -n argocd --timeout=300s
+
+# Get initial password (change immediately!)
+ARGO_PASS=$(kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | base64 -d)
+echo "Argo CD Password: $ARGO_PASS"
+
+
+
 
 #kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj-labs/applicationset/master/manifests/install.yaml
 #kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
